10264bcddc7984ea54eba548b4b1bef2042ff15d
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-07-24 19:10:43 -0500

    MVP

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,4 @@
-const Engineer = require('../lib/Engineer')
+const { Engineer} = require('../lib/Engineer')
 
 
 test("Creates Engineer object", () => {
@@ -7,11 +7,11 @@ test("Creates Engineer object", () => {
 });
 
 test("Function getGitHub() returns GitHub user ID", () => {
-    const engineer = new Engineer("Bob", 999, "bob@bob.com", "bobsgithubid");
+    const engineer = new Engineer("Bob", 999, "bob@bob.com", "bobsgithubid");  
     expect(engineer.getGitHub()).toBe("bobsgithubid");
 });
 
 test("Function getRole() returns Engineer", () => {
-    const engineer = new Engineer
+    const engineer = new Engineer();
     expect(engineer.getRole()).toBe("Engineer")
 });
--- src/user-prompts.js
+++ src/user-prompts.js
@@ -1,67 +1,73 @@
 const inquirer = require('inquirer')
-inquirer.registerPrompt('selectLine', require('inquirer-select-line'));
+const {createEngineer} = require("../lib/Engineer")
+const {createIntern}  = require("../lib/Intern")
+const {createManager} = require("../lib/Manager")
+const templateData = require('../utils/generate-tile')
+const createTemplate = require("../utils/generate-template")
+const { writeFile } = require('../utils/write-files')
 
-const promptManager= data => {
+
+const promptManager = data => {          
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'manager',
+            name: 'name',
             message: "What is the manager's name?",
-            // validate: managerInput => {
-            //   if (managerInput) {
-            //     return true;
-            //   } else {
-            //     console.log('Please enter a name!');
-            //     return false;
-            //   }
-            // }
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter a name!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
-            name: 'managerID',
+            name: 'id',
             message: "What is the manager's employee ID number?",            
-            // validate: (managerIDNumber) => {
-            //   managerIDNumber = managerIDNumber || 0;  
-            //   if (managerIDNumber > 0) {
-            //     return true;                            
-            //   } else {    
-            //     console.log('Please enter an employee ID!');    
-            //     return false;    
+            validate: (idNumber) => {
+              idNumber = idNumber || 0;  
+              if (idNumber > 0) {
+                return true;                            
+              } else {    
+                console.log('Please enter an employee ID!');    
+                return false;    
           
-            //   }
-            // }
+              }
+            }
         },
         {
             type: 'input',
-            name: 'managerEmail',
+            name: 'email',
             message: "What is the manager's email address?",
             default: () => {},
-            // validate: function (managerEmail) {  
-            //     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(managerEmail)        
-            //     if (valid) {                  
-            //         return true;
-            //     } else {
-            //         console.log("Please enter a valid email address!")
-            //          return false;
-            //     }
-            // }
+            validate: function (email) {  
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)        
+                if (valid) {                  
+                    return true;
+                } else {
+                    console.log("Please enter a valid email address!")
+                     return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'managerPhone',
+            name: 'phone',
             message: "What is the manager's telephone number XXX XXX-XXXX ?",
             default: () => {},
-            //     validate: function (managerPhone) {  
-            //         valid = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(managerPhone)        
-            //         if (valid) {                  
-            //             return true;
-            //         } else {
-            //             console.log("Please enter a valid phone number!")
-            //             return false;
-            //         } 
-            //     } 
-         },     
-    ]);
+                validate: function (phone) {  
+                    valid = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(phone)        
+                    if (valid) {                  
+                        return true;
+                    } else {
+                        console.log("Please enter a valid phone number!")
+                        return false;
+                    } 
+                } 
+         },           
+    ]).then(createManager).then(promptAddEmployee);
 };
 
 
@@ -74,142 +80,147 @@ const promptAddEmployee = answer => {
                 name: 'employeeList',                
                 choices: ["Add Engineer", "Add Intern", "Finish Building Team"]
             }
-        ])    
+        ]).then(promptEmployee)    
     };
 
-
-    const addEmployee = async () => {
-        await promptAddEmployee()
-        .then(promptEmployee)    
-    }
-
-    
- 
+      
 const promptEmployee = (answer) => {  
   console.log(answer.employeeList);
-    if(answer.employeeList === "Add Engineer") {
+    if(answer.employeeList === "Add Engineer") {      
           promptEngineer();        
         } else if(answer.employeeList === "Add Intern") {
             promptIntern();
         } else {
-    console.log("generating page")   
-    }
+            const pageData = templateData.tileArray.join("");      
+            const html = createTemplate(pageData);  
+            writeFile(html);            
+        }
 
-    };
-    
-
-
-
+    };  
 
 const promptEngineer = data => {   
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'engineer',
+            name: 'name',
             message: "What is the engineer's name?",
-            // validate: engineerInput => {
-            //     if (engineerInput) {
-            //     return true;
-            //     } else {
-            //     console.log('Please enter a name!');
-            //     return false;
-            //     }
-            // }
-        },
-        
-        {
-            type: 'input',
-            name: 'engineerEmail',
-            message: "What is the engineer's email address?",           
-            // validate: function (engineerEmail) {  
-            //     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(engineerEmail)        
-            //     if (valid) {                  
-            //         return true;
-            //     } else {
-            //         console.log("Please enter a valid email address!")
-            //             return false;
-            //     }
-            // }
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter a name!');
+                return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'engineerID',
+            name: 'id',
             message: "What is the engineer's employee ID number?",            
-            // validate: (engineerIDNumber) => {
-            //     engineerIDNumber = engineerIDNumber || 0;  
-            //     if (engineerIDNumber > 0) {
-            //     return true;                            
-            //     } else {    
-            //     console.log('Please enter an employee ID!');    
-            //     return false;    
+            validate: (idNumber) => {
+               idNumber = idNumber || 0;  
+                if (idNumber > 0) {
+                return true;                            
+                } else {    
+                console.log('Please enter an employee ID!');    
+                return false;    
             
-            //     }
-            // }
+                }
+            }
         },      
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email address?",           
+            validate: function (email) {  
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)        
+                if (valid) {                  
+                    return true;
+                } else {
+                    console.log("Please enter a valid email address!")
+                        return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?",            
+            validate: (githubInput) => {
+                if(githubInput) {               
+                return true;                            
+                } else {    
+                console.log('Please enter a GitHub username!');    
+                return false;    
+            
+                }
+            }
+        },      
+        
           
         
-        ]).then(addEmployee);    
+        ]).then(createEngineer).then(promptAddEmployee);
     };
 
-    const promptIntern = data => {
+    const promptIntern = data => {      
         return inquirer.prompt([
             {
                 type: 'input',
-                name: 'intern',
+                name: 'name',
                 message: "What is the intern's name?",
-                // validate: internInput => {
-                //     if (internInput) {
-                //     return true;
-                //     } else {
-                //     console.log('Please enter a name!');
-                //     return false;
-                //     }
-                // }
+                validate: internInput => {
+                    if (internInput) {
+                    return true;
+                    } else {
+                    console.log('Please enter a name!');
+                    return false;
+                    }
+                }
             },
             {
                 type: 'input',
-                name: 'internID',
+                name: 'id',
                 message: "What is the intern's employee ID number?",            
-                // validate: (internIDNumber) => {
-                //     internIDNumber = internIDNumber || 0;  
-                //     if (internIDNumber > 0) {
-                //     return true;                            
-                //     } else {    
-                //     console.log('Please enter an employee ID!');    
-                //     return false;    
+                validate: (internIDNumber) => {
+                    internIDNumber = internIDNumber || 0;  
+                    if (internIDNumber > 0) {
+                    return true;                            
+                    } else {    
+                    console.log('Please enter an employee ID!');    
+                    return false;    
                 
-                //     }
-                // }
+                    }
+                }
             },
             {
                 type: 'input',
-                name: 'internEmail',
+                name: 'Email',
                 message: "What is the intern's email address?",
                 default: () => {},
-                // validate: function (internEmail) {  
-                //     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(internEmail)        
-                //     if (valid) {                  
-                //         return true;
-                //     } else {
-                //         console.log("Please enter a valid email address!")
-                //             return false;
-                //     }
-                // }
+                validate: function (internEmail) {  
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(internEmail)        
+                    if (valid) {                  
+                        return true;
+                    } else {
+                        console.log("Please enter a valid email address!")
+                            return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'school',
                 message: "What school is the intern attending?",
-                // validate: school => {
-                //     if (school) {
-                //         return true;
-                //     } else {
-                //         console.log("Please enter the intern's school!");
-                //         return false;
-                //     }
-                //     }
+                validate: school => {
+                    if (school) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's school!");
+                        return false;
+                    }
+                    }
                 },              
-            ]).then(addEmployee);
+            ]).then(createIntern).then(promptAddEmployee);
         };
     
-    module.exports = { promptManager, addEmployee, promptEngineer, promptIntern }
+    module.exports = { promptManager, promptEngineer, promptIntern }
